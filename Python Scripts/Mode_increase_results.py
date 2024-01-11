7e91b1a9c6ed08d3dfd7fa882b02375f5caf5432
Glenn James <91394680+glennj258@users.noreply.github.com>
2024-01-11 20:24:34 +1100

    Adding python mode increase scripts and data

---
+++ Python Scripts/Mode_increase_results.py
@@ -0,0 +1,7 @@
+#### Get and combine relavant results for the mode increase, emissions and economic effects from honours development scenarios
+
+# we want 
+
+# - resulting mode share under each development scenario
+
+# - total emissions and economic effects under each scenario (we have these already)
